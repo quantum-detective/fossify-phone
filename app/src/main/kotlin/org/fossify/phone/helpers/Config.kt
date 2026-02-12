23edf1684e21c098ba796be014696c8f6a7f00b5
rostamg <rostamg@proton.me>
2026-02-12 22:39:48 +0300

    Changed default value for call response setting

--- app/src/main/kotlin/org/fossify/phone/helpers/Config.kt
+++ app/src/main/kotlin/org/fossify/phone/helpers/Config.kt
@@ -113,7 +113,7 @@ class Config(context: Context) : BaseConfig(context) {
         set(disableProximitySensor) = prefs.edit().putBoolean(DISABLE_PROXIMITY_SENSOR, disableProximitySensor).apply()
 
     var disableSwipeToAnswer: Boolean
-        get() = prefs.getBoolean(DISABLE_SWIPE_TO_ANSWER, false)
+        get() = prefs.getBoolean(DISABLE_SWIPE_TO_ANSWER, true)
         set(disableSwipeToAnswer) = prefs.edit().putBoolean(DISABLE_SWIPE_TO_ANSWER, disableSwipeToAnswer).apply()
 
     var wasOverlaySnackbarConfirmed: Boolean
